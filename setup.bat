8a4b556c2c1eebfa1d05d83da2dda9b123527ca1
ohioskibidirizz123 <153636980+ohioskibidirizz123@users.noreply.github.com>
2024-08-20 22:48:47 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,13 +1,13 @@
 @echo off
 echo Installing Python packages...
 
-:: Ensure pip is up-to-date
-python -m pip install --upgrade pip
+:: Ensure pip is up-to-date using the py launcher
+py -m pip install --upgrade pip
 
-:: Install the specified Python packages
-pip install times
-pip install colorama
-pip install random-number
+:: Install the specified Python packages using the py launcher
+py -m pip install times
+py -m pip install colorama
+py -m pip install random-number
 
 echo Installation complete.
 pause
