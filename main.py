793203add4b01b5e63e50bbd905ec7ebf4bb027c
ohioskibidirizz123 <153636980+ohioskibidirizz123@users.noreply.github.com>
2024-08-21 21:43:43 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+keyauthapp = api(
+    name = "punchmadeloader", # Application Name
+    ownerid = "3cwS1VOXCS", # Owner ID
+    secret = "613343b7bab7772419b4d98ecea6c96bbffffd4e3d543ea3d46cfe11c86910e1", # Application Secret
+    version = "1.0", # Application Version
+    hash_to_check = getchecksum()
+)
